fdba96e884828baf0ff0d57d3fbef37528310ec9
Supriya Banerjee <204591981+S-Banerjee7@users.noreply.github.com>
2026-07-10 16:46:43 +0530

    Add files via upload

---
+++ com/management/Student.java
@@ -0,0 +1,34 @@
+package com.management;
+
+public class Student {
+    private int id;
+    private String name;
+    private String email;
+    private String course;
+
+    
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+   
+    public Student(int id, String name, String email, String course) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public String getCourse() { return course; }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %d | Name: %s | Email: %s | Course: %s", id, name, email, course);
+    }
+}
---
+++ com/management/StudentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.management;
+
+public class StudentNotFoundException extends Exception {
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
